d4ee313947a257c9c5cdbd5dafd6f55e3a854f86
Moez <65367982+moezmustafa@users.noreply.github.com>
2024-01-02 05:19:48 +0500

    23:43 modals

--- airbnb/app/(tabs)/index.tsx
+++ airbnb/app/(tabs)/index.tsx
@@ -1,11 +1,22 @@
 import { View , Text } from 'react-native';
 import React from 'react';
+import { Link } from 'expo-router';
 
 const Page = () => {
     return (
 
       <View>
         <Text>Page</Text>
+
+        <Link href={"./(modals)/booking"}>
+            Booking
+        </Link>
+        
+        <Link href={"./(modals)/login"}>
+            Login
+        </Link>
+
+        
         </View>
     );
 }
